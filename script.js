2540118e84fc6d3af4354b4d82aadd3725d6584e
Ethan Isola <isolaethan@gmail.com>
2022-01-01 17:27:21 -0600

    Updated the contnents of paragraphs and headings as well as updating styles for lists and paragraphs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "The Ups and Downs of Bits and Bytes";
 document.querySelector("body").appendChild(h2);
 document.body.createElement(h2);
